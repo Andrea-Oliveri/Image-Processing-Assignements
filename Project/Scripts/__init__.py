c03aeb87c28d2ac5aebbbe189f482f4782122db9
aka9 <michaelcjx@yahoo.com.hk>
2021-05-08 23:23:14 +0200

    implement getitem, for specific round and game

---
+++ Project/Scripts/__init__.py
@@ -0,0 +1,7 @@
+from .load import *
+from .utils import *
+from .plots import *
+from .model import *
+#from .extract import *
+from .distortions import *
+#from .train_data_augmentation import *
--- Project/Scripts/load.py
+++ Project/Scripts/load.py
@@ -15,14 +15,39 @@ class Dataset():
 		dfs = self._get_df()
 		self.labels = pd.concat(dfs)
 
-		
-		# in order, read images arrays
-		# call the public func below  (cannot do that on loading, images too large so takes time)
-		self.images = []
 
 	# we can call len(dataset) to return the size
 	def __len__(self):
 		return self.n_games
+
+	# not __getitem__
+	def getitem(self,*game):
+		# args are strings of folder/filenames
+		# essentially getting the image and labels for a specific game and round 
+		# if game one arg -> take entire game
+
+		if not game: return
+
+		spec = [g for g in game]
+		if len(spec) == 1:
+			#get full game
+			g = spec[0]
+			if not(g in os.listdir(self.root)): raise IndexError("file does not exist")
+
+			path = self.root+'/'+g
+			img = np.asarray([cv2.imread(path+'/'+rd) for rd in os.listdir(path) if rd.endswith('.jpg')])
+			label = self.labels.loc[self.labels['game']==g.strip('/')]
+		else:
+			#consider two args
+			g,rd = spec
+			if not(g in os.listdir(self.root)): raise IndexError("file does not exist")
+			path = self.root+'/'+g
+
+			if not(rd in os.listdir(path)): raise IndexError("file does not exist")
+			img = cv2.imread(path+'/'+rd)
+			label = self.labels.loc[(self.labels['game']==g.strip('/')) & (self.labels['round']==int(rd.strip('.jpg')))]
+		#np.array and dataframe
+		return img,label
 
 
 	def _get_df(self):
@@ -39,12 +64,3 @@ class Dataset():
 			dfs.append(df)
 
 		return dfs
-
-	def get_images(self):
-		images = []
-		for game in tqdm(os.listdir(self.root)):
-			path = self.root+game 
-			round_images = [cv2.imread(path+'/'+rd) for rd in os.listdir(path) if rd.endswith('.jpg')]
-			images.append(round_images)
-
-		return np.asarray(images)
