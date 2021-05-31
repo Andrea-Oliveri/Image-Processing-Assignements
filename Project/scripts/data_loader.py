958bbee7bc23088a7288e538cfbff54ce841a6bf
Andrea-Oliveri <andrea.oliveri@epfl.ch>
2021-05-31 17:21:26 +0200

    Changed DataLoader such that it supports missing .csv

--- Project/scripts/data_loader.py
+++ Project/scripts/data_loader.py
@@ -9,7 +9,6 @@ class DataLoader():
 
     def __init__(self, data_dir = os.path.join('.', 'train_games')):
         self.data_dir = data_dir
-        self._load_data()
 
     
     def __getitem__(self, indices):
@@ -20,61 +19,65 @@ class DataLoader():
         
         n_game, n_round = [int(idx) for idx in indices]
         
-        dataframe_row = self.dataframe[(self.dataframe['game'] == n_game) & (self.dataframe['round'] == n_round)]
         
-        if dataframe_row.empty:
-            raise KeyError(f'Round {n_round} in Game {n_game} does not exist in this dataset')
+        # Generating and testing existence of paths for game folder, image round and game csv.
+        game_path = os.path.join(self.data_dir, f'game{n_game}')
         
-        image_path, = dataframe_row['image']
-        image       = cv2.imread(image_path)
+        if not os.path.isdir(game_path):
+            raise ValueError(f"Folder for game {n_game} can't be found in directory {self.data_dir}")
+            
+        image_path = os.path.join(game_path, f'{n_round}.jpg')
         
-        labels = dataframe_row.drop(columns = ['game', 'round', 'image']).to_dict('list')
-        labels = {k: v[0] for k, v in labels.items()}
+        if not os.path.isfile(image_path):
+            raise ValueError(f"Image for round {n_round} can't be found in directory {game_path}")
         
-        return image, labels
-    
+        csv_path = os.path.join(game_path, f'game{n_game}.csv')
 
-    def _load_data(self):
-
-        self.dataframe = []
         
-        for game_dir in os.listdir(self.data_dir):
-            game_path    = os.path.join(self.data_dir, game_dir)
-            game_number, = re.findall(r'(?<=game)\d+', game_dir)
-            
-            # Contains names of images for each round and of labels .csv
-            game_files = os.listdir(game_path)
-            
-            # Separates names of image files and .csv file.
-            game_csv, = [file for file in os.listdir(game_path) if file.endswith('.csv')]
-            game_files.remove(game_csv)
-            
-            # Load image paths.
-            game_image_paths = {}
-            for image_file in game_files:
-                image_path    = os.path.join(game_path, image_file)
-                round_number, = re.findall(r'\d+(?=.jpg)', image_file)
-                
-                game_image_paths[int(round_number)] = os.path.join(image_path)
-                
-            
-            # Load .csv file.
-            game_data = pd.read_csv(os.path.join(game_path, game_csv), delimiter = ',') \
-                          .assign(game = game_number) \
+        # Loading requested data.
+        image = cv2.imread(image_path)
+
+        if os.path.isfile(csv_path):
+            game_data = pd.read_csv(csv_path, delimiter = ',') \
                           .rename(columns = {'Unnamed: 0': 'round'}) \
-                          .astype({'game': int, 'round': int, 'D': int})
-            
-            game_data['image'] = game_data.apply(lambda x: game_image_paths[x['round']], axis = 1)            
-            
-            self.dataframe.append(game_data)
-                   
+                          .astype({'round': int, 'D': int})
         
-        self.dataframe = pd.concat(self.dataframe, axis = 0).sort_values(['game', 'round']).reset_index(drop = True)
+            game_data_row = game_data[game_data['round'] == n_round]
         
+            if game_data_row.empty:
+                raise KeyError(f'Round {n_round} does not exist in {csv_path}')
+                
+            labels = game_data_row.drop(columns = ['round']).to_dict('list')
+            labels = {k: v[0] for k, v in labels.items()}
+            
+            return image, labels
+        
+        return image, None
+    
         
     def get_available_games(self):
-        return np.sort(self.dataframe['game'].unique())
+        game_numbers = []
+        
+        for game_dir in os.listdir(self.data_dir):
+            game_number, = re.findall(r'(?<=game)\d+', game_dir)
+        
+            game_numbers.append( int(game_number) )
+            
+        return sorted(game_numbers)
     
     
     def get_available_rounds(self, n_game):
-        return np.sort(self.dataframe[self.dataframe['game'] == n_game]['round'].unique())
+        game_path = os.path.join(self.data_dir, f'game{n_game}')
+        
+        if not os.path.isdir(game_path):
+            return []
+        
+        image_files   = [file for file in os.listdir(game_path) if file.endswith('.jpg')]
+        round_numbers = []
+        
+        for image_file in image_files:            
+            round_number, = re.findall(r'\d+(?=.jpg)', image_file)
+            
+            round_numbers.append( int(round_number) )
+            
+        return sorted(round_numbers)
