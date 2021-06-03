564188e1e2203f5fe97ed0db4a6f08c51de3f756
Andrea-Oliveri <andrea.oliveri@epfl.ch>
2021-06-03 20:59:47 +0200

    Commented figures suits classifier

--- Project/scripts/figures_suits_classifier.py
+++ Project/scripts/figures_suits_classifier.py
@@ -10,12 +10,30 @@ from .distortions import *
 
 
 class FiguresSuitsClassifier():
+    """
+    Class collecting all necessary models, pre-processing functions and feature extraction steps to be used
+    when predicting suits and figures from images.
+    """
     
     def __init__(self, 
                  figure_classifier_save_path = os.path.join("Models", "Convolutional Network"), 
                  suits_classifier_save_path  = os.path.join("Models", "suits_classifier.pkl"), 
                  figure_classifier_input_resolution = (28, 28)):
+        """
+        Constructor of FiguresSuitsClassifier. Loads the models required to classify the suits and the figures and
+        stores the resolution the net expects as input for later use. Also creates a dictionary converting from
+        scalar labels of the models into string labels.
         
+        Args:
+            figure_classifier_save_path::[str]
+                Path at which the tensorflow model trained to predict the figures in the image is stored.
+            suits_classifier_save_path::[str]
+                Path at which the sklearn model trained to predict the suits in the image is stored.
+            figure_classifier_input_resolution::[tuple]
+                Input resolution of the input to the tensorflow model trained to predict the figures.
+        Returns:
+            None
+        """
         # Load figure and suit classifiers.
         self.figure_classifier = tf.keras.models.load_model(figure_classifier_save_path)
         with open(suits_classifier_save_path, 'rb') as file:
@@ -33,6 +51,19 @@ class FiguresSuitsClassifier():
        
     
     def predict_figure(self, image, color):
+        """
+        Function performing the necessary preprocessing on the figure image, and using a neural network model
+        to classify the figure. 
+        
+        Args:
+            image::[np.array]
+                Image containing the figure we want to preprocess and classify.
+            color::[str]
+                String containing either 'red' or 'black' and describing the color of the figure in the image.
+        Returns:
+            prediction::[str]
+                Prediction of the figure contained in the image.
+        """
         preprocessed_image = self._preprocess_figure(image, color)
         
         prediction = self.figure_classifier.predict(preprocessed_image[None, :, :, None])
@@ -42,17 +73,43 @@ class FiguresSuitsClassifier():
 
 
     def predict_suit(self, image, color):
+        """
+        Function performing the necessary preprocessing on the suit image, computing features for the suits object
+        and classifying the suit.
+        
+        Args:
+            image::[np.array]
+                Image containing the suits we want to preprocess, extract features of and classify.
+            color::[str]
+                String containing either 'red' or 'black' and describing the color of the suit in the image.
+        Returns:
+            prediction::[str]
+                Prediction of the suit contained in the image.
+        """
         preprocessed_image = self.preprocess_suit(image, color)
         
         features = self.get_fourier_descriptor(preprocessed_image, n_coefficients_to_keep = self.n_fourier_coefficients)
                 
         prediction, = self.suits_classifier.predict([features])
         
-        
         return self.suit_lookup[prediction]
     
     
     def _preprocess_figure(self, image, color):
+        """
+        Function performing the necessary preprocessing on the figure image to make it binary, remove noise and 
+        resize it to the net input shape.
+        
+        Args:
+            image::[np.array]
+                Image containing the figure we want to preprocess and later classify.
+            color::[str]
+                String containing either 'red' or 'black' and describing the color of the figure in the image.
+        Returns:
+            mask::[np.array]
+                Binarized version of image containing of shape compatible with net input shape, normalized between
+                0 and 1 and binarized.
+        """
         mask = get_color_pixels(image, color)
 
         height, width = mask.shape
@@ -73,6 +130,18 @@ class FiguresSuitsClassifier():
     
     @staticmethod
     def preprocess_suit(image, color):
+        """
+        Function performing the necessary preprocessing on the suit image to make it binary and remove noise.
+        
+        Args:
+            image::[np.array]
+                Image containing the suits we want to preprocess and later classify.
+            color::[str]
+                String containing either 'red' or 'black' and describing the color of the suit in the image.
+        Returns:
+            mask::[np.array]
+                Binarized version of image containing only the component making up the suit.
+        """
         mask = get_color_pixels(image, color)
         
         # Only take the largest component (except background).
